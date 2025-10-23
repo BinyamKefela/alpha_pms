9de1f1f958111a1663ac08e21b5dd7d0b875ab1e
BinyamKefela <binyamkefela196@gmail.com>
2025-10-23 14:49:02 +0300

    restructured entrpoint

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# entrypoint.prod.sh
+# Apply database migrations
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
+# Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+# Start Gunicorn
 echo "Starting Gunicorn server..."
 gunicorn property_management.wsgi:application --bind 0.0.0.0:8000
